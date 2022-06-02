3606ec7cfa5010413d4324797ade4bc98fa20ef2
UNITEC <Unitec.labcom@gmail.com>
2022-06-02 14:47:42 -0500

    Se realizo ejercico

---
+++ Assets/Scripts_2/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+  public int vida;
+    public string tipo;
+    public float velocidad;
+
+    public void Ataque()
+    {
+      
+    }
+
+    public void Movimiento()
+    {
+       
+    }
+}
